58639372881e11cfdd7c6ae6d1d2e27b00336a37
Gwepura <aketani32@gmail.com>
2025-04-25 10:14:05 +0200

    User Profile

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,11 +1,12 @@
-import { formatDate } from '@/lib/utils'
+import { cn, formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { Author, Startup } from '@/sanity/types'
-
+import { Skeleton } from './ui/skeleton'
+// import {}
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author}
 
 function StartupCard({ post }: { post: StartupTypeCard }) {
@@ -47,8 +48,8 @@ function StartupCard({ post }: { post: StartupTypeCard }) {
                 </div>
                 <Link href={`/user/${author?._id}`}>
                     <Image 
-                        src="https://placehold.co/48x48"
-                        alt="placeholder"
+                        src={author?.image}
+                        alt={author?.name}
                         width={48}
                         height={48}
                         className='rounded-full'
@@ -75,6 +76,18 @@ function StartupCard({ post }: { post: StartupTypeCard }) {
                 </Button>
             </div>
         </li>
+    )
+}
+
+export const StartUpCardSkeleton = () => {
+    return (
+        <>
+            {[0, 1, 2, 3, 4].map((index: number) => (
+                <li key={cn("skeleton", index)}>
+                    <Skeleton className='startup-card_skeleton'/>
+                </li>
+            ))}
+        </>
     )
 }
 
